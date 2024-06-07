1f9398bb8846f16d62f3a96c04adadcdc29ca7f3
BALAJI T <112803946+Balaji-04@users.noreply.github.com>
2024-06-07 10:51:46 +0530

    Create counterApp.vue

---
+++ counterApp.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+let count = ref(0);
+let custom_number = ref(0);
+
+function inc(){
+  count.value++;
+}
+
+function reset(){
+  count.value = this.custom_number;
+}
+</script>
+
+<template>
+  <h1>{{ count }}</h1>
+  <button @click="inc()">Increment</button>
+  <br>
+  <input v-model="custom_number" placeholder="Custom number to reset" value="0"/>
+  <button @click="reset()">Reset</button>
+</template>
